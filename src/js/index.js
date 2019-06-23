f47bb2cde35c709660306845c2f069909f14c2bd
VincentCanCode <49010435+VinnyCanCode@users.noreply.github.com>
2019-06-23 00:18:19 -0400

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+// // Smooth Scrolling
+// $('.nav_bar a, .btn').on('click', function(event) {
+//   if (this.hash !== '') {
+//     event.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html, body').animate(
+//       {
+//         scrollTop: $(hash).offset().top
+//       },
+//       800
+//     );
+//   }
+// });
+
+
+<!-- JQuery CDN -->
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+
+
+<!-- LOCAL JS FILE -->
+
+<script type="text/javascript" src="../src/js/index.js"></script>
